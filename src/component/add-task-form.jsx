14a38897ebd65670fd38453ab9d15687f33f365e
Sailesh Patel <saileshapatel@gmail.com>
2022-02-28 21:33:18 +0000

    Add style and information to task form component
    * Add text to tell users to input text and add task to list
    * Add styling to add-task-form component

--- src/component/add-task-form.jsx
+++ src/component/add-task-form.jsx
@@ -15,6 +15,7 @@ const TaskForm = (props) => {
     return (
         <div id="add-task-form">
             <h2>Add task:</h2>
+            <p>Enter a task you want to record below, then click the button to add it to the list.</p>
             <input type="text" name="task-name" value={taskName} onChange={(e) => setTaskName(e.target.value)} />
             <button onClick={addItemToList} >Add Task</button>
         </div>
